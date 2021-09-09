d39e35e1ab9fe935ca4419018a7c8eac4f8a97a8
jianli588 <82671025+jianli588@users.noreply.github.com>
2021-09-09 11:22:42 -0400

    Update Test.java

--- src/Start/Test.java
+++ src/Start/Test.java
@@ -3,7 +3,7 @@ package Labyrinth;
 
 public class Test {
 
-	 static Thread t1 = new Thread(new Labyrinth.GameFrame());
+	static Thread t1 = new Thread(new Labyrinth.GameFrame());
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
